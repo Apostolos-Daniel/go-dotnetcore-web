ff6ba691c36aff6399da11403949a003ecfe2414
Apostolos-Daniel <tolisapostolidis@gmail.com>
2019-01-20 16:42:48 +0000

    added home handler to handlers.go file

--- golang-webapp/cmd/web/main.go
+++ golang-webapp/cmd/web/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-	"html/template"
 	"log"
 	"net/http"
 )
 
+// use pattern matching for .go files to run both .go files in web folder. In Windows, use PowerShell for this:
+// go run $(ls cmd\web\*.go | % {$_.FullName})
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
@@ -16,30 +17,4 @@ func main() {
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
-}
-
-func home(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/" {
-		http.NotFound(w, r)
-		return
-	}
-
-	files := []string{
-		"./ui/html/home.page.tmpl",
-		"./ui/html/base.layout.tmpl",
-		"./ui/html/footer.partial.tmpl",
-	}
-
-	ts, err := template.ParseFiles(files...)
-	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
-		return
-	}
-
-	err = ts.Execute(w, nil)
-	if err != nil {
-		log.Println(err.Error())
-		http.Error(w, "Internal Server Error", 500)
-	}
 }
